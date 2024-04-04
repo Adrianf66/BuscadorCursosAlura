c4460da37ce44fb664488f81a5ba338fb1bb7123
Adrian Oliveira <adrian.ferrassini@gmail.com>
2024-04-04 13:52:06 -0300

    Ajustado classe pessoa, classe titular e arquivo teste

--- src/Pessoa.php
+++ src/Pessoa.php
@@ -4,8 +4,8 @@ namespace Alura\BuscadorDeCursos;
 
 class Pessoa
 {
-    public $nome;
-    public $idade;
+    protected string $nome;
+    protected int $idade;
 
 
     public function __construct(int $idade, string $nome)
@@ -13,4 +13,16 @@ class Pessoa
         $this->idade = $idade;
         $this->nome = $nome;
     }
+
+    public function informaNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function informaIdade(): int
+    {
+        return $this->idade;
+    }
+
+
 }
